e2899e4305b38fab11e9b960ef4de8d18611bc00
harjotsingh <harjot39@ymail.com>
2016-02-17 01:41:05 +0530

    Added apache.conf

--- setup.php
+++ setup.php
@@ -2,7 +2,12 @@
 echo "Setting up Files\n";
 exec("sudo chmod +x apachange");
 exec("sudo chmod +x apachange.php");
+exec("sudo chmod 777 /etc/apache2/sites-available/000-default.conf");
+exec("sudo chmod 777 /etc/apache2/apache2.conf");
 exec("sudo cp -n apachange /usr/bin");
 exec("sudo cp -n apachange.php /usr/bin");
+$apache=file_get_contents('apache.conf');
+$file='/etc/apache2/apache2.conf';
+file_put_contents($file,$apache);
 echo "You are Ready to Use the Command\n";
 ?>
