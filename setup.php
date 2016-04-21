a65b3dc41d9f23c782c48c82030923dcedf35ea7
harjotsingh <harjot39@ymail.com>
2016-04-21 15:23:37 +0530

    Changed setup.php to overwrite apachange.php

--- setup.php
+++ setup.php
@@ -5,7 +5,7 @@ exec("sudo chmod +x apachange.php");
 exec("sudo chmod 777 /etc/apache2/sites-available/000-default.conf");
 exec("sudo chmod 777 /etc/apache2/apache2.conf");
 exec("sudo cp -n apachange /usr/bin");
-exec("sudo cp -n apachange.php /usr/bin");
+exec("sudo \cp  apachange.php /usr/bin");
 $apache=file_get_contents('apache.conf');
 $file='/etc/apache2/apache2.conf';
 file_put_contents($file,$apache);
